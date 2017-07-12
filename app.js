5af993c486519de20038b0e64159ca43302f225e
ziedYazidi <ziedyazidi75@gmail.com>
2017-07-12 13:43:17 +0200

    Continuoues integration

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World!, this is a test for continuoues integration');
+	
 }).listen(8080);
