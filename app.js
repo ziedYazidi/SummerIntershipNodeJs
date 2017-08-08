34b847a94bc6cac1149e44bb27d5474510ee2a8c
ziedYazidi <ziedyazidi75@gmail.com>
2017-08-08 22:44:52 +0200

    CD

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //Setting up the port
-app.set('port',process.env.PORT || 80);
+app.set('port',process.env.PORT || 1919);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
