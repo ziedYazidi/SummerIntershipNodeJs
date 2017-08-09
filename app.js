9b259b14047fba5a4d9a5a61aac5de7002ee2d99
ziedYazidi <ziedyazidi75@gmail.com>
2017-08-09 12:40:31 +0200

    CD

--- app.js
+++ app.js
@@ -16,19 +16,17 @@ var app = express();
 var serve = http.createServer(app);
 var io = require('socket.io')(serve);
 var socket= io;
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-//Setting up the port
-app.set('port',process.env.PORT || 1919);
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 app.get('/edit', function(req, res){
     res.render('edit-form');
 });
